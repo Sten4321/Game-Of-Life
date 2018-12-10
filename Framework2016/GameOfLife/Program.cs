5b6790b1603d41c9f549b420f957c0e048c12a86
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-12-10 14:23:58 +0100

    Fixes

--- Framework2016/GameOfLife/Program.cs
+++ Framework2016/GameOfLife/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,19 @@ namespace GameOfLife
         [STAThread]
         static void Main()
         {
+            CreateDirectory();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        public static void CreateDirectory()
+        {
+            string path = Environment.CurrentDirectory;
+
+            if (!Directory.Exists(path+"\\plugins"))
+            {
+                Directory.CreateDirectory(path + "\\plugins");
+            }
         }
     }
 }
