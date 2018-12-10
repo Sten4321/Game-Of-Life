9af32b5e912872b4d325da3d4d19081d824a306a
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-12-10 14:46:57 +0100

    Mortens projekt added

---
+++ Framework2016/Morten_AI/FatherFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+using AIFramework;
+using AIFramework.Actions;
+using AIFramework.Entities;
+
+namespace Morten_AI
+{
+    public class RandomAgentFactory : AgentFactory
+    {
+        public override Agent CreateAgent(IPropertyStorage propertyStorage)
+        {
+            return new WhoIsYourFatherAndWhatDoesHeDo(propertyStorage);
+        }
+
+        public override Agent CreateAgent(Agent parent1, Agent parent2, IPropertyStorage propertyStorage)
+        {
+            
+            return new WhoIsYourFatherAndWhatDoesHeDo(propertyStorage);
+        }
+
+        public override Type ProvidedAgentType
+        {
+            get { return typeof(WhoIsYourFatherAndWhatDoesHeDo); }
+        }
+
+        public override string Creators
+        {
+            get { return "Morten!"; }
+        }
+
+        public override void RegisterWinners(List<Agent> sortedAfterDeathTime)
+        {
+            //Do data collection - Perhaps used to evolutionary algoritmen
+        }
+    }
+}
